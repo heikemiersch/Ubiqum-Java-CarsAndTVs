2ef5282bb803b28cee0890bf246d56dc310744d6
Heike Miersch <heike.miersch@hotmail.de>
2019-12-03 14:50:55 +0100

    first commit

--- src/Car.java
+++ src/Car.java
@@ -1,24 +1,14 @@
 public class Car {
 
-    //    state components / fields (in contrast to parameters
-    //    below in parentheses that are passed into variable)
+    //    state components / fields (in contrast to parameters below in parentheses that are passed into variable)
     private String brand;
     private String model;
     private int speed;
     private int amount;
 
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-//    constructor
+    //    constructor
     public Car(String brand, String model, int speed) {
-//        "this" refers to the field in the class,
-//        by parameter field is updated
+//        "this" refers to the field in the class, by parameter field is updated
         this.brand = brand;
         this.model = model;
         this.speed = speed;
@@ -27,11 +17,9 @@ public class Car {
     public String getBrand() {
         return brand;
     }
-
     public String getModel() {
         return model;
     }
-
     public int getSpeed() {
         return speed;
     }
@@ -50,12 +38,22 @@ public class Car {
     public void setSpeed(int speed) {
         this.speed = speed;
     }
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 
     public void accelerate(int amount) {
         this.speed += amount;
     }
 
     public void brake(int amount) {
-        while (this.speed > 0) this.speed -= amount;
+        if (this.speed > 0 && this.speed >= amount) {this.speed -= amount;} else {
+            this.speed = 0;
+        }
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
         Car ferrari = new Car("ferrari", "portofino", 0);
         Car jag = new Car("jaguar", "e-type", 0);
         ferrari.accelerate(20);
-        ferrari.brake(50);
+        ferrari.accelerate(120);
+        ferrari.brake(160);
+        jag.accelerate(120);
+        jag.brake(30);
         System.out.println(ferrari.getBrand() + " " + ferrari.getModel() + " running at " + ferrari.getSpeed() + " km/h");
+        System.out.println(jag.getBrand() + " " + jag.getModel() + " running at " + jag.getSpeed() + " km/h");
 
         System.out.println("Hello World.");
     }
